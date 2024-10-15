64fd48b509d65478bbd5c266823e04a21db424bc
gerzon <gerzon@everneat.co>
2024-10-16 01:16:55 +0800

    all

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,14 @@ const allowedOrigins = [
 // };
 // Custom CORS middleware
 
-app.use("*", cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
